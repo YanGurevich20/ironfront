7618534aa1bddb4c99bf6bae772bca344f78032a
yangurevich20 <yan.gurevich20@gmail.com>
2025-05-15 23:17:26 +0300

    file error

--- ui/garage/upgrade_section/upgrade_section.gd
+++ ui/garage/upgrade_section/upgrade_section.gd
@@ -11,13 +11,11 @@ class_name UpgradeSection extends VBoxContainer
 @onready var equipment_upgrade_list: VBoxContainer = %EquipmentUpgradeList
 
 func _ready() -> void:
-    modules_button.pressed.connect(func()->void: _view_upgrade_list(modules_upgrade_list))
-    crew_button.pressed.connect(func()->void: _view_upgrade_list(crew_upgrade_list))
-    equipment_button.pressed.connect(func()->void: _view_upgrade_list(equipment_upgrade_list))
+	modules_button.pressed.connect(func()->void: _view_upgrade_list(modules_upgrade_list))
+	crew_button.pressed.connect(func()->void: _view_upgrade_list(crew_upgrade_list))
+	equipment_button.pressed.connect(func()->void: _view_upgrade_list(equipment_upgrade_list))
 
 func _view_upgrade_list(list_to_show: VBoxContainer) -> void:
-  for list: VBoxContainer in [modules_upgrade_list, crew_upgrade_list, equipment_upgrade_list]:
-    list.visible = false
-  list_to_show.visible = true
-
-  upgrade_list_container.visible = true
+	for list: VBoxContainer in [modules_upgrade_list, crew_upgrade_list, equipment_upgrade_list]:
+		list.visible = list == list_to_show
+	upgrade_list_container.visible = true
