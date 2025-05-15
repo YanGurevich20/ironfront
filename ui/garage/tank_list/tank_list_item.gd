ba222994511bf466314279092edfad9e5d74229d
yangurevich20 <yan.gurevich20@gmail.com>
2025-05-15 23:15:37 +0300

    garage progress

---
+++ ui/garage/tank_list/tank_list_item.gd
@@ -0,0 +1,8 @@
+extends PanelContainer
+
+@export var tank_name: String
+@export var tank_image: Texture2D
+
+func _ready() -> void:
+	$TankListItemLabel.text = tank_name
+	$TankListItemTexture.texture = tank_image
---
+++ ui/garage/upgrade_section/upgrade_section.gd
@@ -0,0 +1,23 @@
+class_name UpgradeSection extends VBoxContainer
+
+@onready var modules_button: Button = %ModulesButton
+@onready var crew_button: Button = %CrewButton
+@onready var equipment_button: Button = %EquipmentButton
+
+@onready var upgrade_list_container: VBoxContainer = %UpgradeListContainer
+
+@onready var modules_upgrade_list: VBoxContainer = %ModulesUpgradeList
+@onready var crew_upgrade_list: VBoxContainer = %CrewUpgradeList
+@onready var equipment_upgrade_list: VBoxContainer = %EquipmentUpgradeList
+
+func _ready() -> void:
+    modules_button.pressed.connect(func()->void: _view_upgrade_list(modules_upgrade_list))
+    crew_button.pressed.connect(func()->void: _view_upgrade_list(crew_upgrade_list))
+    equipment_button.pressed.connect(func()->void: _view_upgrade_list(equipment_upgrade_list))
+
+func _view_upgrade_list(list_to_show: VBoxContainer) -> void:
+  for list: VBoxContainer in [modules_upgrade_list, crew_upgrade_list, equipment_upgrade_list]:
+    list.visible = false
+  list_to_show.visible = true
+
+  upgrade_list_container.visible = true
