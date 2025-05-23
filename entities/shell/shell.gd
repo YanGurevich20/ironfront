b7c51059c488cfc3a691d517c662a8e66cff1ea0
yangurevich20 <yan.gurevich20@gmail.com>
2025-05-23 20:47:14 +0300

    remove editor path from git config, add shell tracers

--- entities/shell/shell.gd
+++ entities/shell/shell.gd
@@ -5,11 +5,17 @@ var firing_tank: Node2D
 var damage: int
 var velocity: Vector2
 var shell_texture: Texture2D
+var is_tracer: bool
 
 @onready var shell_sprite: Sprite2D = $Sprite2D
+@onready var fire_particles: GPUParticles2D = %FireParticles
+@onready var tracer_particles: GPUParticles2D = %TracerParticles
 
 func _ready() -> void:
 	shell_sprite.texture = shell_texture
+	body_entered.connect(_on_body_entered)
+	fire_particles.emitting = !is_tracer
+	tracer_particles.emitting = is_tracer
 
 func initialize(_shell_id: ShellManager.ShellId, muzzle: Marker2D, _firing_tank: Node2D) -> void:
 	var shell_spec: ShellSpec = ShellManager.get_shell_spec(_shell_id)
@@ -20,6 +26,7 @@ func initialize(_shell_id: ShellManager.ShellId, muzzle: Marker2D, _firing_tank:
 	position = muzzle.global_position
 	velocity = Vector2.RIGHT.rotated(rotation) * shell_spec.muzzle_velocity
 	shell_texture = shell_spec.base_shell_type.projectile_texture
+	is_tracer = shell_spec.base_shell_type.is_tracer
 
 func _physics_process(delta: float)->void:
 	position += velocity * delta
